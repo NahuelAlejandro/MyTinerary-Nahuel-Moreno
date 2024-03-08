e0f25ad487fad2beff38754bde4a35655bb61332
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-08 16:00:10 -0300

    se cambia la separacion de imagenes del carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,7 @@ const Carousel =({popularCities})=>{
     
     return(
 
-        <div className="flex flex-wrap justify-between gap-3 md:gap-6 w-11/12">
+        <div className="flex flex-wrap justify-around gap-3 md:gap-6 w-11/12">
             <CarouselItem city={popularCities[0]}/>
             <CarouselItem city={popularCities[1]}/>
             <CarouselItem city={popularCities[2]}/>
