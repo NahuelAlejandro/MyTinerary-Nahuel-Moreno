aaa2690973b05a49e8b4f5490b002b2cdaceaa79
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-12 13:18:13 -0300

    se agrega la ruta de city

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import  Home  from "./views/Home";
 import  Cities  from "./views/Cities";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import City from "./views/City";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path ="/" element = {<Home/>}/>
           <Route path ="Cities" element = {<Cities/>}/>
+          <Route path ="Cities/:id" element = {<City/>}/>
         </Routes>
       </BrowserRouter>
     
