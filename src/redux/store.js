969fde852214848e1b3aafb845371cbc281fca8d
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-21 22:51:08 -0300

    se agrega user al store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import citiesReducer from "./reducers/citiesReducer";
+import userReducer from "./reducers/usersReducer";
 
 export default configureStore({
     reducer:{
-        citiesStore: citiesReducer
+        citiesStore: citiesReducer,
+        users: userReducer
     }
 })
