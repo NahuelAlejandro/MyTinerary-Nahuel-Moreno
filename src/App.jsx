ed26a25aefc8e13ae050c2b33b4bdf0cd83bbb21
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-21 23:03:50 -0300

    se agrega un dispatch parar que cunado se recargue la pagina siga logueado el usuario

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,39 @@ import  Home  from "./views/Home";
 import  Cities  from "./views/Cities";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import City from "./views/City";
-import store from "./redux/store";
-import { Provider } from "react-redux";
-
+import { useDispatch } from "react-redux";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import authQueries from "./sevice/authQueries";
+import alerts from "./utils/alerts";
+import { logIn } from "./redux/actions/userActions";
 
 function App() {
+  const dispatch = useDispatch()
  
+  useEffect(()=>{
+    authQueries.loginWithToken().then((response)=>{
+      if(response.status == 200){
+        dispatch(logIn(response.data))
+        alerts.success(`welcome ` + response.data.first_name)
+      }
+    });
+  },[])
 
   return (
    
       <BrowserRouter>
-        <Provider store={store}>
           <Routes>
             <Route path ="/" element = {<Home/>}/>
             <Route path ="Cities" element = {<Cities/>}/>
             <Route path ="Cities/:id" element = {<City/>}/>
+            <Route path ="/SignUp" element = {<SignUp/>}/>
+            <Route path ="/Login" element = {<Login/>}/>
           </Routes>
-        </Provider>
+        <ToastContainer/>
       </BrowserRouter>
     
   )
