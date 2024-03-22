bab5f321becfa821a7f77f3a62f6d880d0804399
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-22 16:17:17 -0300

    se arregla reedireccion al hacer click en las cards de cities

--- src/components/CitiesCards.jsx
+++ src/components/CitiesCards.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const CitiesCards = ({city}) => {
 
-    function handleClick(e) {
-        
-      location.href = `/cities/${e.target.parentElement.id}`
-        
-    }
+  
 
   return (
-            <article className='w-full h-48 relative overflow-hidden border-2 border-sky-600 rounded cursor-pointer md:h-80 md:w-[30%] lg:h-96 duration-500 transition-all group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100' id={city._id} onClick={handleClick}>
+          <Link className='w-full h-48 relative overflow-hidden border-2 border-sky-600 rounded cursor-pointer md:h-80 md:w-[30%] lg:h-96 duration-500 transition-all group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100' to={`/cities/${city._id}`}>
+            <article className='h-full w-full' id={city._id} >
                 <span className='h-full w-full absolute z-10'></span>
                 <img className='w-full h-full object-cover' src={`${city.image}`} alt={`${city.name}`}/>
                 <footer className='bg-black/55 w-full absolute bottom-0 flex flex-col items-center font-semibold '>
@@ -17,6 +15,7 @@ const CitiesCards = ({city}) => {
                     <h2 className='text-sky-200 '>{city.name}</h2>
                 </footer>
             </article>
+          </Link>
   )
 }
 
