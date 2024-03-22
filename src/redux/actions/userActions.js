9c3b8967378d5dbe627ccacc70941aae90bd277a
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-21 23:17:08 -0300

    se crean las acciones logOut y logIn

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,15 @@
+import { createAction } from "@reduxjs/toolkit";
+
+
+export const logIn = createAction ("logIn",(user)=>{
+    return {
+        payload:user
+    }
+})
+
+export const logOut = createAction("logOut",()=>{
+    localStorage.removeItem("token");
+    return{
+        payload:{}
+    }
+})
